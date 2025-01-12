c8f82fa405854f972c31fac1525fd6486816738b
TerrifyingBridge <chrijgre@umich.edu>
2025-01-11 22:44:26 -0500

    transit

--- Chapter 1 Two Body Problem/1.6 Orbital Elements for Exoplanets/transit_test.py
+++ Chapter 1 Two Body Problem/1.6 Orbital Elements for Exoplanets/transit_test.py
@@ -7,65 +7,90 @@ import numpy as np
 
 def au_to_star_rad(dist: float, star: tbs.AstroBody) -> float:
     temp = dist * c.PhysicalConstants.au
-    return temp / star.radius
+    return temp / (star.radius*1000)
 
 
 b1 = tbs.AstroBody(c.Solar.mass, c.Solar.radius)
 b2 = tbs.AstroBody(c.Jupiter.mass, c.Jupiter.reference_radius)
 
-system = tbs.TwoBodySystem(b1, b2, 1, 0.25, np.pi / 2, np.pi/2)
+system = tbs.TwoBodySystem(b1, b2, 1, 0.25, np.pi / 2.0039, np.pi)
 
-time = np.linspace(0, system.period, 100)
+time = np.linspace(0, system.period, 100000)
 system.fill_path_list(time)
 
-fig = plt.figure(figsize=(7, 7), layout="constrained")
-spec = fig.add_gridspec(2, 2)
-ax = fig.add_subplot(spec[:, 0], projection="3d")
-ax1 = fig.add_subplot(spec[0, 1])
-ax2 = fig.add_subplot(spec[1, 1])
+temp_trans = []
+temp = []
+consec = 0
+for i in range(len(system.body2_pos_x)):
+    x = system.body2_pos_x[i] * c.PhysicalConstants.au
+    y = system.body2_pos_y[i] * c.PhysicalConstants.au
+    z = system.body2_pos_z[i]
+    r_planet = system.body2.radius
+    r_star = system.body1.radius
+    if x**2 + y**2 <= (r_planet*1000 + r_star*1000)**2 and z >= 0:
+        consec += 1
+        temp.append(i)
+        if (i == len(system.body2_pos_x) - 1):
+            temp_trans.append(temp)
+    else:
+        if (consec != 0):
+            temp_trans.append(temp)
+            temp = []
+        consec = 0
 
-line1, = ax.plot([], [], [])
-line2, = ax.plot([], [], [])
-line3, = ax.plot([], [], [], "bo")
-line4, = ax.plot([], [], [], "r.")
-max_val = 0.25 + system.semi_major_axis * (1 + system.eccentricity) / c.PhysicalConstants.au
+print(temp_trans)
+
+if len(temp_trans) == 2:
+    if (temp_trans[0][0] < temp_trans[1][0]):
+        second_list = temp_trans[1].copy()
+        temp_trans.pop(1)
+        temp_trans.insert(0, second_list)
+    temp_trans = temp_trans[0] + temp_trans[1]
+elif (len(temp_trans) == 1):
+    temp_trans = temp_trans[0]
+
+print()
+print(temp_trans)
+
+transit_x = []
+transit_y = []
+
+for index in temp_trans:
+    transit_x.append(system.body2_pos_x[index])
+    transit_y.append(system.body2_pos_y[index])
+
+fig = plt.figure(figsize=(7, 7))
+ax1 = fig.add_subplot(1, 2, 1)
+ax1.set_aspect("equal")
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.set_aspect("equal")
 
 star_radius = b1.radius
-circle1 = plt.Circle((0, 0), 1.0, color="b")
-circle2 = plt.Circle((-100, -100), b2.radius / b1.radius, color="r")
-ax1.add_patch(circle1)
-ax1.add_patch(circle2)
+max_val = 1 + b2.radius / b1.radius
+max_val *= 1.1
+dpv = fig.get_figwidth() * fig.get_dpi() / (max_val*2)
+print(dpv)
+line1, = ax1.plot([], [], marker=".", markersize=dpv * b2.radius / b1.radius)
+line2, = ax1.plot([0], [0], marker=".", markersize=dpv)
 
 
 def init():
-    ax.set(xlim=(-max_val, max_val), ylim=(-max_val, max_val), zlim=(-max_val, max_val))
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-
-    ax1.set(xlim=(-1.2, 1.2), ylim=(-1.2, 1.2))
-    return line1, line2, line3, line4, circle2
+    ax1.set(xlim=(-max_val, max_val), ylim=(-max_val, max_val))
+    return line1,
 
 
 def update(step):
-    index = step + 1
-    temp1 = system.body1_pos_x[:index]
-    temp2 = system.body1_pos_y[:index]
-    temp3 = system.body1_pos_z[:index]
-    temp4 = system.body2_pos_x[:index]
-    temp5 = system.body2_pos_y[:index]
-    temp6 = system.body2_pos_z[:index]
+    x = transit_x[:step+1].copy()
+    y = transit_y[:step+1].copy()
 
-    line1.set_data_3d(temp1, temp2, temp3)
-    line2.set_data_3d(temp4, temp5, temp6)
-    line3.set_data_3d([temp1[-1]], [temp2[-1]], [temp3[-1]])
-    line4.set_data_3d([temp4[-1]], [temp5[-1]], [temp6[-1]])
+    for i in range(len(x)):
+        x[i] = au_to_star_rad(x[i], b1)
+        y[i] = au_to_star_rad(y[i], b1)
 
-    circle2.center = (au_to_star_rad(temp4[-1], b1), au_to_star_rad(temp5[-1], b1))
-    return line1, line2, line3, line4, circle2
+    line1.set_data([x[-1]], [y[-1]])
+    return line1,
 
 
-ani = FuncAnimation(fig, update, frames=len(time), init_func=init, interval=50, blit=True)
-# ani.save(filename="assets/bad_transit.gif", writer="pillow")
-fig.tight_layout(pad=3.0)
+ani = FuncAnimation(fig, update, frames=len(temp_trans), init_func=init, interval=50, blit=True)
+# ani.save(filename="assets/better_transit.gif", writer="pillow")
 plt.show()
