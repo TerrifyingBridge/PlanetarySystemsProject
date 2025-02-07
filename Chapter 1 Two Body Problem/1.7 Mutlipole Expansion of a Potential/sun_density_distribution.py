79972cc421591679a9f24151a0789beea15a18ed
TerrifyingBridge <chrijgre@umich.edu>
2025-02-07 14:56:16 -0500

    plots

--- Chapter 1 Two Body Problem/1.7 Mutlipole Expansion of a Potential/sun_density_distribution.py
+++ Chapter 1 Two Body Problem/1.7 Mutlipole Expansion of a Potential/sun_density_distribution.py
@@ -24,10 +24,33 @@ def exp2(x):
     return 165 * math.pow(10, -3.16 * x)
 
 
-line_x1 = [(i / 99) for i in range(100)]
-line_y1 = [exp2(i) for i in line_x1]
+def line1(x):
+    return -630*x + 155
+
+def line2(x):
+    return (11.87 - 30.5*x) / 0.15
+
+def line3(x):
+    return (7.2 - 7.2*x) / 0.75
+
+line_x1 = [(i / 99) for i in range(19)]
+line_y1 = [line1(i) for i in line_x1]
+
+line_x2 = [(i / 99) for i in range(19, 36)]
+line_y2 = [line2(i) for i in line_x2]
+
+line_x3 = [(i / 99) for i in range(36, 100)]
+line_y3 = [line3(i) for i in line_x3]
+
+for i in range(len(x)):
+    print(x[i], y[i])
+
+print()
+print((line_x2[-1], line_y2[-1]))
 
 plt.scatter(x, y)
-# plt.plot(line_x1, line_y1)
-# plt.yscale("log")
+plt.plot(line_x1, line_y1, c="red")
+plt.plot(line_x2, line_y2, c="orange")
+plt.plot(line_x3, line_y3, c="yellow")
+plt.yscale("log")
 plt.show()
